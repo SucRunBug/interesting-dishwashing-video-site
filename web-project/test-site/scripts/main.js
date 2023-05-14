1b276cdea62d83fb797921063b714cd50a7464c9
SucRunBug <yyanfeiyu@163.com>
2023-05-14 13:02:03 +0800

    add a photo of dishwasher, add some js to allow user input user name and show it on the page

--- web-project/test-site/scripts/main.js
+++ web-project/test-site/scripts/main.js
@@ -1,6 +1,29 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/washing-maid.png') {
+      myImage.setAttribute('src', 'images/dishwasher.jpeg');
+    } else {
+      myImage.setAttribute('src', 'images/washing-maid.png');
+    }
+}
+
+let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
-document.querySelector("html").addEventListener("click", function () {
-    alert("别戳我，我怕疼。");
-  });
-  
+function setUserName() {
+    let myName = prompt('please input your name.');
+    localStorage.getItem('name', myName);
+    myHeading.textContent = "Make washing cool! " + myName;
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = "Make washing cool! " + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
